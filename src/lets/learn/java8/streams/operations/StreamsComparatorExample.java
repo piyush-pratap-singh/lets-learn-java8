ce9f703937c5979d071d0052ccda82f0a21b45b9
piyush-pratap-singh <piyushpratap1707@gmail.com>
2021-08-08 23:42:39 +0530

    "Stream operation sorted(Comparator) example"

---
+++ src/lets/learn/java8/streams/operations/StreamsComparatorExample.java
@@ -0,0 +1,21 @@
+package lets.learn.java8.streams.operations;
+
+import lets.learn.java8.data.Student;
+import lets.learn.java8.data.StudentDatabase;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class StreamsComparatorExample {
+
+    public static void main(String[] args){
+
+       List<Student> studentNames = StudentDatabase.getAllStudents().stream()
+                .sorted(Comparator.comparing(Student::getName))
+                .collect(Collectors.toList());
+
+       System.out.println("Students sorted by name" + studentNames);
+
+    }
+}
